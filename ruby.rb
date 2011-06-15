2b70418ba5f6c112a0f0399e27f1e541032ae410
madlep <julian.doherty.ml@gmail.com>
2011-06-15 17:20:28 +1000

    install bundler, awesome_print as default RVM gems

--- ruby.rb
+++ ruby.rb
@@ -1,5 +1,6 @@
 dep 'ruby' do
-  requires  'rvm', 
+  requires  'rvm',
+            'rvm default gems',
             '1.9.2 installed.rvm',
             '1.8.7 installed.rvm',
             '1.9.2 default.rvm', 
@@ -10,6 +11,10 @@ meta :rvm do
   def rvm args
     shell("export PATH=~/bin:$PATH; rvm #{args}", :log => args['install'], :spinner => true)
   end
+  
+  template {
+    requires 'rvm default gems'
+  }
 end
 
 dep '1.8.7 installed.rvm' do
@@ -45,4 +50,25 @@ dep 'rvm' do
     append_to_file(rvm_session_init, bash_profile)
     shell rvm_session_init
   }
+end
+
+meta :rvm_default_gem do
+  template {
+    def default_gems_file
+      File.join(home, ".rvm", "gemsets", "default.gems")
+    end
+  
+    met? { grep basename, default_gems_file }
+    meet { append_to_file basename, default_gems_file }
+  }
+end
+
+$rvm_default_gems = []
+["bundler", "awesome_print"].each do |gem_name|
+  dep "#{gem_name}.rvm_default_gem"
+  $rvm_default_gems << gem_name
+end
+
+dep "rvm default gems" do
+  requires $rvm_default_gems.map{|gem_name| "#{gem_name}.rvm_default_gem"}
 end
