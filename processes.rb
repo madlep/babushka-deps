d6901494d9bfe173faa373886214adde349bbb44
madlep <julian.doherty.ml@gmail.com>
2011-07-21 09:54:39 +1000

    disable spotlight indexing

--- processes.rb
+++ processes.rb
@@ -1,3 +1,18 @@
+dep 'processes' do
+  requires 'disable spotlight indexing'
+end
+
+# use quicksilver for the same thing instead
+dep 'disable spotlight indexing' do
+  met? {
+    `mdutil -s /` =~ /Indexing disabled/
+  }
+  
+  meet {
+    sudo "mdutil -a -i off"
+  }
+end
+
 def ensure_not_running(process_executable)
   unless raw_shell("ps aux | grep #{process_executable} | grep 'grep' --invert-match").stdout.empty? 
     log "#{process_executable} is running. Need to stop first"
